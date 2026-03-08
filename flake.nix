9e215490ba879efb67160c1db0f91c6b06d0bc36
Nathan Kidd <nathankidd@hey.com>
2026-03-08 06:56:42 +0000

    added devShell

--- flake.nix
+++ flake.nix
@@ -141,6 +141,9 @@
         ];
       };
 
+      # ── Dev shell ──────────────────────────────────────────────────
+      devShells.${system}.default = openkrill.devShells.${system}.default;
+
       # ── Image outputs ─────────────────────────────────────────────
       packages.${system} = {
         qcow2           = images.qcow2.image;
